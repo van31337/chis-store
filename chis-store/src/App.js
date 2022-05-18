9fc2f5c2a35ca3865f5ab245cd0081f6da331c7e
Van Tran <tranquocvan2402@gmail.com>
2022-05-18 05:09:38 -0600

    frontpage

---
+++ chis-store/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.componenet";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
